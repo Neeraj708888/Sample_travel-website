41a014d2a12fb6636106b2c376a1845aa3607dc6
Neeraj Kumar <amck.sharma@gmail.com>
2026-03-18 17:28:53 +0530

    DB file updated

--- app/backend/src/config/db.ts
+++ app/backend/src/config/db.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabase_url = process.env.DB_URL
-const supabase_key = process.env.DB_KEY
-
-if (!supabase_url || !supabase_key) {
-    throw new Error("Supabase credentials missing in environment variables")
-}
+const supabase_url = process.env.DB_URL!
+const supabase_key = process.env.DB_KEY!
 
 export const db = createClient(supabase_url, supabase_key)
